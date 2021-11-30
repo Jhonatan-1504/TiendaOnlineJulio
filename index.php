64e30fc9493723eefb77846ecb1135c3acb97669
Jhonatan-1504 <Jhonatan.Valenzuela.19@outlook.com>
2021-11-30 16:51:30 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="La mejor tienda online improvisada en 10 horas">
+  <title>Tienda online Julio</title>
+</head>
+<body>
+
+  <h1>pinche enzo</h1>
+
+</body>
+</html>
