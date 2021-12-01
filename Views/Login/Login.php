1fdc2018b77a2ac243480043330c2fa87ca89e9b
Julio Diaz <juliocesardiazmorales@hotmail.com>
2021-11-30 20:59:57 -0500

    Login add

--- Views/Login/Login.php
+++ Views/Login/Login.php
@@ -10,10 +10,52 @@
   <link rel="stylesheet" href="./assets/styles.css">
   <title>Login</title>
 </head>
-    <h1>Hola mundo</h1>
 <body>
 
-    
+    <!--  -->
+    <section class="vh-100 gradient-custom">
+  <div class="container py-3 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-1 mt-md-1 ">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Por favor introduce correo y contraseña!</p>
+
+              <div class="form-outline form-white mb-4">
+              <label class="form-label" for="typeEmailX">Correo:</label>
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+               
+              </div>
+
+              <div class="form-outline form-white mb-4">
+              <label class="form-label" for="typePasswordX">Contraseña:</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+               
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Se te olvidó tu contraseña?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5  m-2" type="submit">Inicia sesión</button>
+
+            
+
+            </div>
+
+            <div>
+              <p class="mb-0">No tengo una cuenta? <a href="#!" class="text-white-50 fw-bold">Registrate</a></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <!--  -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
