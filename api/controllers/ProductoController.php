b27b2229d236097e4948a5da6308f196002de3f3
Jhonatan-1504 <Jhonatan.Valenzuela.19@outlook.com>
2021-12-02 01:45:16 -0500

    agregando API productos

--- api/controllers/ProductoController.php
+++ api/controllers/ProductoController.php
@@ -1,50 +1,146 @@
 <?php
 
-if(!isset($_GET['option'])) {
+if (!isset($_GET['option'])) {
   echo "<h1>No tienes permitido entrar a esta pagina</h1> <a href='http://localhost/TiendaOnlineJulio/'>Volver al Menu</a>";
   die();
 }
 
+if (!isset($_POST)) {
+  echo "No hay datos asignados";
+  die();
+}
+
+$table = "calero";
+
 $option = $_GET['option'];
 
+include_once "../config/config.php";
+include_once "../models/database.php";
 include_once "../models/Productos.php";
 
 switch ($option) {
-  case 'add':
-    Create();
+  case 'addProduct':
+    CrearProducto();
     break;
-  case "edit":
-    Edit();
+  case "updateProduct":
+    ActualizarProducto();
     break;
-  case "update":
-    Update();
-    break;
-  case "delete":
-    Deletes();
-    break;
-  case "list";
-    Index();
+  case "deleteProduct":
+    BorrarProducto();
     break;
 }
 
-// path "./api/controllers/ProductoController.php?option=list"
-function Index()
-{
+
+// HTTP_POST
+/*
+  http://localhost/TiendaOnlineJulio/api/controllers/ProductoController.php?option=addProduct
   
+  FormData
+  {
+    "nombre":"Fuckboy",
+    "descripcion":"Hora fuckboy poster",
+    "stock":"15",
+    "precio":"10.00",
+    "idEmpleado":"1",
+    "imagen": FILE,
+  }
+*/
+function CrearProducto()
+{
+  // $request = json_decode(file_get_contents("php://input"));
+  $request = json_decode(json_encode($_POST));
+
+  $fileName = SaveFile('imagen', 'products');
+
+  $datos = [
+    "Nombre_Producto" => $request->nombre,
+    "Descripcion_Producto" => $request->descripcion,
+    "Imagen_Producto" => $fileName,
+    "Stock_Producto" => $request->stock,
+    "Precio_Producto" => $request->precio,
+    "ID_Empleado" => $request->idEmpleado
+  ];
+
+  $reg = new Productos();
+  $result = $reg->Create($datos, 6);
+  if ($result) {
+    echo "Producto registrado";
+  }
 }
 
-function Create()
+// HTTP_POST
+/*
+  http://localhost/TiendaOnlineJulio/api/controllers/ProductoController.php?option=updateProduct&id=1
+  
+  FormData
+  {
+    "nombre":"Fuckboy",
+    "descripcion":"Hora fuckboy poster",
+    "stock":"15",
+    "precio":"10.00",
+    "idEmpleado":"1",
+    "imagen": FILE,
+  }
+*/
+function ActualizarProducto()
 {
+  $request = json_decode(json_encode($_POST));
+
+  $fileName = SaveFile('imagen', 'products');
+
+  $datos = [
+    "Nombre_Producto" => $request->nombre,
+    "Descripcion_Producto" => $request->descripcion,
+    "Imagen_Producto" => $fileName,
+    "Stock_Producto" => $request->stock,
+    "Precio_Producto" => $request->precio,
+    "ID_Empleado" => $request->idEmpleado
+  ];
+
+  $reg = new Productos();
+  $reg->Where(["ID_Producto" => $_GET['id']]);
+  $result = $reg->Update($datos);
+  if ($result) {
+    echo "Producto actualizado";
+  }
 }
 
-function Update()
+// HTTP_GET
+/*
+  http://localhost/TiendaOnlineJulio/api/controllers/ProductoController.php?option=deleteProduct&id=1
+*/
+function BorrarProducto()
 {
+  $reg = new Productos();
+  $reg->Where(["ID_Producto" => $_GET['id']]);
+  $result = $reg->Delete();
+  if ($result) {
+    echo "Borrado con exito";
+  }
 }
 
-function Deletes()
+// HELPER
+function SaveFile(string $key, string $dir)
 {
-}
+  $directorio = "../../assets/$dir/";
 
-function Edit()
-{
+  $fileName = $_FILES[$key]['name'];
+  $fileSave = $_FILES[$key]['tmp_name'];
+
+  if (!file_exists($directorio)) {
+    mkdir($directorio, 0777, true);
+    if (file_exists($directorio)) {
+      if (move_uploaded_file($fileSave, $fileName)) {
+        return $fileName;
+      } else {
+        return "Error";
+      }
+    }
+  } else {
+    if (move_uploaded_file($fileSave, $directorio . $fileName)) {
+      return $fileName;
+    } else {
+      return "Error";
+    }
+  }
 }
