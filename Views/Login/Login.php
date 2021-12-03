74c01ad031d23f102d86f41df44f1608dbd1edaa
Julio Diaz <juliocesardiazmorales@hotmail.com>
2021-12-03 00:34:31 -0500

    .

--- Views/Login/Login.php
+++ Views/Login/Login.php
@@ -18,6 +18,7 @@
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
+        <form id="formRegister">
           <div class="card-body p-5 text-center">
 
             <div class="mb-md-1 mt-md-1 ">
@@ -27,17 +28,17 @@
 
               <div class="form-outline form-white mb-4">
               <label class="form-label" for="typeEmailX">Correo:</label>
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+              <input name="email" type="email" id="typeEmailX" class="form-control form-control-lg" required />
                
               </div>
 
               <div class="form-outline form-white mb-4">
               <label class="form-label" for="typePasswordX">Contraseña:</label>
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+              <input minlength="6" name="password" type="password" id="typePasswordX" class="form-control form-control-lg" required/>
                
               </div>
 
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Se te olvidó tu contraseña?</a></p>
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="../Login/RePassword.php">Se te olvidó tu contraseña?</a></p>
 
               <button class="btn btn-outline-light btn-lg px-5  m-2" type="submit">Inicia sesión</button>
 
@@ -46,10 +47,11 @@
             </div>
 
             <div>
-              <p class="mb-0">No tengo una cuenta? <a href="#!" class="text-white-50 fw-bold">Registrate</a></p>
+              <p class="mb-0">No tengo una cuenta? <a href="../Login/Register.php" class="text-white-50 fw-bold">Registrate</a></p>
             </div>
 
           </div>
+        </form>
         </div>
       </div>
     </div>
