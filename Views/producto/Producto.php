f576ed467d845505eb9d506ec986cb6dfb2c1bcd
Enzo Vargas <enzo.vp@outlook.com>
2021-12-03 16:52:16 -0500

    Se obtiene los datos por medio de su id

--- Views/producto/Producto.php
+++ Views/producto/Producto.php
@@ -18,24 +18,24 @@
             <div class="card-body">
                 <div style="display:flex;flex-direction: column;">
                     <div style="text-align: center;">
-                        <h2><strong>Audifonos</strong></h2>  
-                        <img style="width: 40%" src="../../assets/products/airpods.webp" alt="" class="card-img-top"/> 
+                        <h2><strong id="nombre_producto"></strong></h2>  
+                        <img id="imagen_producto" style="width: 40%"  alt="" class="card-img-top"/> 
                     </div>
-                    <div style="width: 100%;">
+                    <div style="width: 100%;margin: 20px 0;">
                         <div class="accordion" id="accordionPanelsStayOpenExample">
-                                    <div class="accordion-item">
-                                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                            Descripcion
-                                        </button>
-                                        </h2>
-                                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                                        <div class="accordion-body">
-                                            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque neque optio, maiores culpa sed repellendus architecto nobis aliquid eligendi, voluptatibus veritatis nulla laudantium excepturi tempore obcaecati inventore adipisci reprehenderit facilis.</p>
-                                        </div>
-                                        </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                        Descripcion
+                                    </button>
+                                    </h2>
+                                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                    <div class="accordion-body">
+                                        <p id="description_producto"></p>
+                                    </div>
                                     </div>
                                 </div>
+                            </div>
                         <div style="margin-top: 20px;display: flex;align-items: center;justify-content: space-between;">
                             <div class="dropdown">
                                 <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
@@ -50,7 +50,7 @@
                                     <li class="dropdown-item" value="6">6</li>
                                 </ul>
                             </div>
-                            <strong style="color: red;">Precio: S/ 190.00</strong>
+                            <strong style="color: red;" id="precio_producto">Precio: S/ 190.00</strong>
                         </div>
                     </div>
                 </div>
@@ -61,7 +61,25 @@
             </div>
         </div>
 </div>
-    
+    <script>
+        const url = new URL(document.URL);
+        const idProducto = url.searchParams.get("idProducto");
+
+        const teodio = async ()=>{
+            const response = await fetch(`http://localhost:8080/TiendaOnlineJulio/api/controllers/ProductoController.php?option=editProduct&idProducto=${idProducto}`)
+            const data = await response.json();
+            render(data[0]);
+        }
+
+        teodio();
+
+        const render = (data) =>{
+            document.getElementById("nombre_producto").textContent = data.Nombre_Producto;
+            document.getElementById("imagen_producto").src = `../../assets/products/${data.Imagen_Producto}`;
+            document.getElementById("description_producto").textContent = data.Descripcion_Producto;
+            document.getElementById("precio_producto").textContent = data.Precio_Producto;
+        }
+    </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
