82c6105651fc209c3a5b9c923ca24ae58788e8c0
Julio Diaz <juliocesardiazmorales@hotmail.com>
2021-12-03 23:04:13 -0500

    con-Adm

---
+++ Views/LoginAdmin/LoginAdm.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="La mejor tienda online improvisada en 10 horas">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="./assets/styles.css">
+  <title>Login</title>
+</head>
+<body>
+
+    <!--  -->
+    <section class="vh-100 gradient-custom">
+  <div class="container py-3 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card text-white" style="border-radius: 1rem;background:#1B212C">
+        <form id="formRegister">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-1 mt-md-1 ">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login ADM</h2>
+              <p class="text-white-50 mb-5">ADM CORPORATION</p>
+
+              <div class="form-outline form-white mb-4">
+              <label class="form-label" for="typeEmailX">Correo:</label>
+              <input name="email" type="email" id="typeEmailX" class="form-control form-control-lg" required />
+               
+              </div>
+
+              <div class="form-outline form-white mb-4">
+              <label class="form-label" for="typePasswordX">Contraseña:</label>
+              <input minlength="6" name="password" type="password" id="typePasswordX" class="form-control form-control-lg" required/>
+               
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="../LoginAdmin/RepassAdm.php">Se te olvidó tu contraseña?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5  m-2" type="submit">Inicia sesión</button>
+
+            
+
+            </div>
+
+            <div>
+              <p class="mb-0">No tengo una cuenta? <a href="../LoginAdmin/RegisterAdm.php" class="text-white-50 fw-bold">Registrate</a></p>
+            </div>
+            <div id="mesa-war" class="alert alert-danger d-none m-3" role="alert">
+                        Los Datos no son Validos!
+                    </div>
+          </div>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <!--  -->
+    <script>
+    const Form = document.getElementById("formRegister");
+    Form.addEventListener("submit", HandleSubmit)
+
+    async function HandleSubmit(event) {
+        event.preventDefault();
+        let MisDatos = new FormData(Form);
+        let response = await fetch(
+            'http://localhost/TiendaOnlineJulio/api/controllers/UsuarioController.php?option=verificar', {
+                method: "POST",
+                body: MisDatos
+            });
+         if(response.status != 200){
+            const mesaWar = document.getElementById("mesa-war");
+            mesaWar.classList.remove("d-none");
+         }else return window.location.href = "../../index.php";
+
+    }
+    </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ api/controllers/EmpleadoController.php
@@ -0,0 +1,190 @@
+<?php
+
+if (!isset($_GET['option'])) {
+  echo "<h1>No tienes permitido entrar a esta pagina</h1> <a href='http://localhost/TiendaOnlineJulio/'>Volver al Menu</a>";
+  die();
+}
+
+if (!isset($_POST)) {
+  echo "No hay datos asignados";
+  die();
+}
+
+$option = $_GET['option'];
+
+include_once "../config/config.php";
+include_once "../models/database.php";
+include_once "../models/Empleado.php";
+
+switch ($option) {
+  case "perfil":
+    BuscarIdEmpleado();
+  break;
+  case 'verificar':
+    VerificarEmpleado();
+    break;
+  case 'addUser':
+    CrearNuevoEmpleado();
+    break;
+  case "updatePassword":
+    ActualizarPassword();
+    break;
+  case "updateAllData":
+    ActualizarAll();
+    break;
+  case "deleteUser":
+    BorraCuenta();
+    break;
+}
+
+// HTTP_GET
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=perfil&idUser=1
+*/
+function BuscarIdEmpleado(){
+  $reg = new Empleado();
+  $reg->Where(["ID_Empleado"=>$_GET['idUser']]);
+  $result = $reg->All();
+  if($result === null){
+    http_response_code(404);
+    echo json_encode(["msg"=>"No existe Empleado"]);
+    die();
+  }
+  echo json_encode($result[0]);
+}
+
+
+// HTTP_POST
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=verificar
+{
+  "email":'PincheENZO',
+  "password":'12345'
+}
+*/
+function VerificarEmpleado()
+{
+  $request = json_decode(json_encode($_POST));
+
+  $reg = new Empleado();
+  $reg->Verification($request->email, $request->password);
+  $user = $reg->All();
+  if ($user !== null) {
+    http_response_code(200);
+    echo json_encode($user);
+  } else {
+    http_response_code(404);
+    echo json_encode(['msg' => 'No existe']);
+  }
+}
+
+// HTTP_POST
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=addUser
+
+{
+    "nameUser":"PincheENZO",
+    "password":"123"
+}
+*/
+function CrearNuevoEmpleado()
+{
+  $request = json_decode(json_encode($_POST));
+
+  $datos = [
+    "Email_Empleado" => $request->nameUser,
+    "Contraseña_Empleado" => $request->password
+    
+  ];
+
+  $reg = new Empleado();
+  $result = $reg->CreateUser($datos, 2);
+
+  if ($result) {
+    echo "Registrado correctamente";
+  };
+}
+
+// HTTP_POST
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=updatePassword
+
+  {
+    "email":"pINCHEeNZO",
+    "password":"12345678"
+  }
+*/
+function ActualizarPassword()
+{
+  $request = json_decode(json_encode($_POST));
+
+  $datos = ["Contrasena_Empleado" => $request->password];
+
+  $reg = new Empleado();
+  $reg->Select(["ID_Empleado"]);
+  $reg->Where(["Email_Empleado" => $request->email]);
+  $user = $reg->All();
+
+  if ($user === null) {
+    http_response_code(404);
+    echo json_encode(["msg" => "Correo no existe"]);
+    die();
+  }
+
+  http_response_code(200);
+  $result = $reg->UpdateUser($datos);
+
+  if ($result) {
+    echo "Actualizado con exito";
+  }
+}
+
+// HTTP_POST
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=updateAllData&id=1
+
+  {
+    "dni":"87456321",
+    "correo":"PincheEnzo@gmail.com",
+    "nombre":"Piña",
+    "apellido":"Enzo",
+    "telefono":"987456321"
+  }
+*/
+function ActualizarAll()
+{
+  $request = json_decode(file_get_contents("php://input"));
+
+  $datos = [
+    "DNI_Empleado" => $request->dni,
+    "Email_Empleado" => $request->correo,
+    "Nombres_Empleado" => $request->nombre,
+    "Apellidos_Empleado" => $request->apellido,
+    "Telefono_Empleado" => $request->telefono
+  ];
+
+  $reg = new Empleado();
+  $reg->Where(["ID_Empleado" => $_GET['id']]);
+  $result = $reg->UpdateUser($datos);
+
+  if ($result) {
+    echo "Actualizado con exito";
+  }
+}
+
+// HTTP_GET
+/*
+http://localhost/TiendaOnlineJulio/api/controllers/EmpleadoController.php?option=deleteUser&id=1
+*/
+
+function BorraCuenta()
+{
+  $reg = new Empleado();
+
+  $reg->Where(["ID_Empleado" => $_GET['id']]);
+  $result = $reg->DeleteUser();
+
+  if ($result) {
+    echo "Borrado con exito";
+  }
+}
