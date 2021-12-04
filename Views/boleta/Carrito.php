91d2ffb784351fa54690952ec254d553028667d7
Enzo Vargas <enzo.vp@outlook.com>
2021-12-03 22:56:21 -0500

    Se agrego funcionalidad

--- Views/boleta/Carrito.php
+++ Views/boleta/Carrito.php
@@ -27,7 +27,7 @@
                     <div style="display: flex;align-items: center;justify-content: space-between;">
                     <div>
                       <input type="text" value="1" size="1" maxlength="1" disabled/>
-                      <strong style="color:red;">S/ 190.00</strong>
+                      <strong style="color:red;" id="precio_producto">S/ 190.00</strong>
                     </div>
                       <button class="btn btn-dark">Eliminar</button>
                     </div>
@@ -57,7 +57,7 @@
             <h3 style="font-size: 18px;text-align:center;">Resumen de Compras</h3>
         </div>
         <div class="list-group-item" style="display:flex;justify-content: space-between;">
-          Subtotal (2) <strong style="color:red;">S/ 190.00</strong>
+          Subtotal (2) <span style="color:red;">S/ <strong id="subtotal">100.00</strong></span>
         </div>
         <div class="list-group-item" style="display:flex;justify-content: space-between;">
           Envio <strong>Envio Gratis</strong>
@@ -66,19 +66,22 @@
           IGV <strong>18%</strong>
         </div>
         <div class="list-group-item" style="display:flex;justify-content: space-between;font-size:20px">
-          Total a pagar <strong style="color:red;">S/ 290.00</strong>
+          Total a pagar <span style="color:red;">S/ <strong id="total"></strong></span>
         </div>
         <div class="list-group-item">
             <button class="btn btn-dark" style="width: 100%;" type="submit">Finalizar Compra</button>
         </div>
       </div>
     </div>
-    <script>
-      const valores = localStorage.getItem("carrito");
-      console.log(valores)
-    </script>
   </div>
+  <script>
+    document.getElementById("subtotal").textContent = 
+    const subtotal = parseInt(document.getElementById("subtotal").textContent);
+    const IGV = 0.18;
+    const total = subtotal + Math.round(subtotal * IGV)
+    document.getElementById("total").innerHTML = total;
 
+  </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
