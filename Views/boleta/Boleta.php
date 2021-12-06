7b1cae1e67e6cf314e12b3f2474c9c699d4aab38
Jhonatan-1504 <Jhonatan.Valenzuela.19@outlook.com>
2021-12-06 00:17:25 -0500

    diseño boleta

--- Views/boleta/Boleta.php
+++ Views/boleta/Boleta.php
@@ -11,46 +11,31 @@
   <link rel="stylesheet" href="./assets/styles.css">
   <title>Login</title>
 </head>
+
 <body>
 
   <?php include_once "../../api/config/config.php"; ?>
   <?php include_once "../../components/header.php"; ?>
 
-  <div class="container">
-    <div style="margin: 5%;">
-      <div  class="row list-group-item" style="display:flex;align-items: end;background: #000;color: #fff">
-        <h1 class="col-md">Detalle de Boleta</h1>
-        <div class="col-md">
-          <p  style="font-size:15px;justify-content: space-around;display: flex;align-items: center;">Fecha de Compra: 10/02/2021 <strong style="font-size: 1.5rem">Total: 200.00</strong> </p>
-        </div> 
+  <div class="d-flex justify-content-between bg-dark text-white p-3 position-relative" style="height: 10rem;">
+    <h1>Detalle de Boleta</h1>
+    <div class="d-flex flex-column align-items-end justify-content-end">
+      <h2>S/.200</h2>
+      <h6>20/02/2021 15:05:09</h6>
+    </div>
+  </div>
+
+  <div class="container mt-3">
+    <div class="d-flex row border border-1 border-dark bg-dark text-white">
+      <div class="col-10 p-2">Detalles</div>
+      <div class="col-2 p-2">Precio</div>
+    </div>
+    <div class="d-flex row border border-1">
+      <div class="col-10 border-end p-2">
+        <img src="../../assets/products/airpods.webp" class="" alt="">
       </div>
-      <div class=" list-group-item">
-        <div class="row">
-          <div class="col-md-4" style="text-align: center;">
-            <img style="width:80%;" src="../../assets/products/airpods.webp" alt="" class="card-img-top"/> 
-          </div>
-          <div class="col-md-8">
-            <h2 style="font-size: 20px"><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit.</strong></h2>  
-            <div style="margin: 50px 0;" class="accordion" id="accordionPanelsStayOpenExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                        Descripcion
-                    </button>
-                    </h2>
-                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                    <div class="accordion-body">
-                        <p id="description_producto"></p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div style="display: flex;align-items: center;">
-              <input style="padding: 3px;font-size: 1.2rem;" type="text" value="1" size="1" maxlength="1" disabled/>
-              <strong style="color:red;font-size: 1.5rem;margin-left: 20px;">S/ 190.00</strong>
-            </div>
-          </div>
-        </div>
+      <div class="col-2 p-2">
+        S/.200
       </div>
     </div>
   </div>
